e657122e7aaff7d2c3c777a115232ab6970a2e88
Kevin Mulholland <kevin.mulholland@yahoo.com>
2020-05-19 16:13:43 -0400

    Add new Flash and Event to window

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// We need an event bus to push more notifications to our list of notifications
+// We can hook up Vuex to do the same thing if we want.
+window.Event = new Vue();
+
+// Flash messaging
+window.flash = function(
+  title,
+  body = null,
+  type = "success",
+  important = false,
+  display_time = 3000
+) {
+  window.Event.$emit("flash", title, body, type, important, display_time);
+}; // flash('my new flash message')
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
